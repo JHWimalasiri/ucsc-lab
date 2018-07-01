bc8d118ea317634d338bbe45e68df82f04ed5f77
JHWimalasiri <hwimalasiri@gmail.com>
2018-07-01 06:01:24 +0530

    Update layout

---
+++ src/app/components/add-lab/add-lab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-lab',
+  templateUrl: './add-lab.component.html',
+  styleUrls: ['./add-lab.component.css']
+})
+export class AddLabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
